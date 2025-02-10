15333e33d7bd81c35758c26b2388ba80a89a41c8
FelipeMartins <felipemrts@icloud.com>
2025-02-10 02:54:28 -0400

    change popups to main

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,4 @@
-import { useState } from "react";
-
-import Popup from "./components/Popup/Popup.jsx";
-import NewCard from "./components/Popup/components/NewCard/NewCard";
-import EditAvatar from "./components/Popup/components/EditAvatar/EditAvatar";
-import EditProfile from "./components/Popup/components/EditProfile/EditProfile";
-
 function Header() {
-  const [popup, setPopup] = useState(null);
-  const newCardPopup = { title: "Novo cartão", children: <NewCard /> };
-  const editAvatarPopup = { title: "Editar avatar", children: <EditAvatar /> };
-  const editProfilePopup = {
-    title: "Editar perfil",
-    children: <EditProfile />,
-  };
-
-  function handleOpenPopup(popup) {
-    setPopup(popup);
-  }
-
-  function handleClosePopup() {
-    setPopup(null);
-  }
-
   return (
     <header className="header">
       <div className="header__logo">
@@ -30,37 +7,6 @@ function Header() {
           className="header__logo-item"
         />
       </div>
-      <div className="header__profile">
-        <div className="header__avatar-edit">
-          <img
-            src="#"
-            alt="Foto de perfil do usuário"
-            className="header__avatar"
-            onClick={() => handleOpenPopup(editAvatarPopup)}
-          />
-        </div>
-        <div className="header__profile-edit">
-          <div className="header__wrap">
-            <h1 className="header__title">Jacques Costeau</h1>
-            <button
-              className="header__edit-button"
-              alt="Um botão com uma canela, simbolizando a edição!"
-              onClick={() => handleOpenPopup(editProfilePopup)}
-            />
-          </div>
-          <p className="header__subtitle">Explorador</p>
-        </div>
-        <button
-          className="header__add"
-          alt="Um botão com um simbolo de mais, simbolizando a adição de mais fotos!"
-          onClick={() => handleOpenPopup(newCardPopup)}
-        />
-      </div>
-      {popup && (
-        <Popup onClose={handleClosePopup} title={popup.title}>
-          {popup.children}
-        </Popup>
-      )}
     </header>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
+
 import Card from "./components/Card/Card";
+import Popup from "./components/Popup/Popup.jsx";
+import NewCard from "./components/Popup/components/NewCard/NewCard";
+import EditAvatar from "./components/Popup/components/EditAvatar/EditAvatar";
+import EditProfile from "./components/Popup/components/EditProfile/EditProfile";
 
 function Main() {
+  const [popup, setPopup] = useState(null);
+  const newCardPopup = { title: "Novo cartão", children: <NewCard /> };
+  const editAvatarPopup = { title: "Editar avatar", children: <EditAvatar /> };
+  const editProfilePopup = {
+    title: "Editar perfil",
+    children: <EditProfile />,
+  };
+
+  function handleOpenPopup(popup) {
+    setPopup(popup);
+  }
+
+  function handleClosePopup() {
+    setPopup(null);
+  }
   const cards = [
     {
       isLiked: false,
@@ -22,15 +43,44 @@ function Main() {
   console.log(cards);
 
   return (
-    <>
-      <main className="content">
-        <section className="cards">
-          {cards.map((card) => (
-            <Card key={card._id} card={card} />
-          ))}
-        </section>
-      </main>
-    </>
+    <main className="content">
+      <div className="content__profile">
+        <div className="content__avatar-edit">
+          <img
+            src="#"
+            alt="Foto de perfil do usuário"
+            className="content__avatar"
+            onClick={() => handleOpenPopup(editAvatarPopup)}
+          />
+        </div>
+        <div className="content__profile-edit">
+          <div className="content__wrap">
+            <h1 className="content__title">Jacques Costeau</h1>
+            <button
+              className="content__edit-button"
+              alt="Um botão com uma canela, simbolizando a edição!"
+              onClick={() => handleOpenPopup(editProfilePopup)}
+            />
+          </div>
+          <p className="content__subtitle">Explorador</p>
+        </div>
+        <button
+          className="content__add"
+          alt="Um botão com um simbolo de mais, simbolizando a adição de mais fotos!"
+          onClick={() => handleOpenPopup(newCardPopup)}
+        />
+      </div>
+      <section className="cards">
+        {cards.map((card) => (
+          <Card key={card._id} card={card} />
+        ))}
+      </section>{" "}
+      {popup && (
+        <Popup onClose={handleClosePopup} title={popup.title}>
+          {popup.children}
+        </Popup>
+      )}
+    </main>
   );
 }
 
